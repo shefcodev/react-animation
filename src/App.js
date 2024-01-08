cf19d10bb28c9678101bbaa7305a1f67fe6a3e3f
shefcodev <sherefjimoh@gmail.com>
2024-01-08 11:49:46 +0100

    wrapped  transition component

--- src/App.js
+++ src/App.js
@@ -65,12 +65,8 @@ class App extends Component {
             ></div>
           )}
         </Transition>
-        {this.state.modalIsOpen ? (
-          <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        ) : null}
-        {this.state.modalIsOpen ? (
-          <Backdrop show={this.state.modalIsOpen} />
-        ) : null}
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+        {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className='Button' onClick={this.showModal}>
           Open Modal
         </button>
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import Transition from 'react-transition-group/Transition';
 
 import './Modal.css';
 
 const modal = ({ show, closed }) => {
-  const cssStyles = ['Modal', show ? 'ModalOpened' : 'ModalClosed'];
+  const cssStyles = ['Modal'];
 
   return (
-    <div className={cssStyles.join(' ')}>
-      <h1>A Modal</h1>
-      <button className='Button' onClick={closed}>
-        Dismiss
-      </button>
-    </div>
+    <Transition in={show} timeout={300} mountOnEnter unmountOnExit>
+      {(state) => {
+        const cssStyles = [
+          'Modal',
+          state === 'entering'
+            ? 'ModalOpened'
+            : state === 'exiting'
+            ? 'ModalClosed'
+            : null,
+        ];
+
+        return (
+          <div className={cssStyles.join(' ')}>
+            <h1>A Modal</h1>
+            <button className='Button' onClick={closed}>
+              Dismiss
+            </button>
+          </div>
+        );
+      }}
+    </Transition>
   );
 };
 export default modal;
